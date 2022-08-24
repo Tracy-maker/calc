cd3c6fc1f4f8ecf366e7f5d95663b4fd7bcd55a0
Ying <ydlvns@gmail.com>
2022-08-24 21:19:32 +1000

    click on the num of function

--- js/calc.js
+++ js/calc.js
@@ -3,3 +3,13 @@ function init(){
     num.value=0;
     disabled="disabled";
 }
+function num_1_click(){
+  var num = document.getElementById("num");
+  var n =num.value;
+  if (n=="0"){
+    n="1";
+  }else{
+    n=n+"1";
+  }
+  document.getElementById("num").value=n;
+}
