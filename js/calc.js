78ff18c5b58f9d27828d40cd0b3c7bdbda3f3110
Ying <ydlvns@gmail.com>
2022-08-25 11:18:04 +1000

    fix some bug

--- js/calc.js
+++ js/calc.js
@@ -1,15 +1,105 @@
-function init(){
-    var num= document.getElementById("num");
-    num.value=0;
-    disabled="disabled";
-}
-function num_1_click(){
+function init() {
   var num = document.getElementById("num");
-  var n =num.value;
-  if (n=="0"){
-    n="1";
-  }else{
-    n=n+"1";
+  num.value = 0;
+  disabled = "disabled";
+}
+function num_1_click() {
+  var num = document.getElementById("num");
+  var n = num.value;
+  if (n == "0") {
+    n = "1";
+  } else {
+    n = n + "1";
   }
-  document.getElementById("num").value=n;
+  document.getElementById("num").value = n;
+}
+function num_2_click() {
+  var num = document.getElementById("num");
+  var n = num.value;
+  if (n == "0") {
+    n = "2";
+  } else {
+    n = n + "2";
+  }
+  document.getElementById("num").value = n;
+}
+function num_3_click() {
+  var num = document.getElementById("num");
+  var n = num.value;
+  if (n == "0") {
+    n = "3";
+  } else {
+    n = n + "3";
+  }
+  document.getElementById("num").value = n;
+}
+function num_4_click() {
+  var num = document.getElementById("num");
+  var n = num.value;
+  if (n == "0") {
+    n = "4";
+  } else {
+    n = n + "4";
+  }
+  document.getElementById("num").value = n;
+}
+function num_5_click() {
+  var num = document.getElementById("num");
+  var n = num.value;
+  if (n == "0") {
+    n = "5";
+  } else {
+    n = n + "5";
+  }
+  document.getElementById("num").value = n;
+}
+function num_6_click() {
+  var num = document.getElementById("num");
+  var n = num.value;
+  if (n == "0") {
+    n = "6";
+  } else {
+    n = n + "6";
+  }
+  document.getElementById("num").value = n;
+}
+function num_7_click() {
+  var num = document.getElementById("num");
+  var n = num.value;
+  if (n == "0") {
+    n = "7";
+  } else {
+    n = n + "7";
+  }
+  document.getElementById("num").value = n;
+}
+function num_8_click() {
+  var num = document.getElementById("num");
+  var n = num.value;
+  if (n == "0") {
+    n = "8";
+  } else {
+    n = n + "8";
+  }
+  document.getElementById("num").value = n;
+}
+function num_9_click() {
+  var num = document.getElementById("num");
+  var n = num.value;
+  if (n == "0") {
+    n = "9";
+  } else {
+    n = n + "9";
+  }
+  document.getElementById("num").value = n;
+}
+function num_1_click() {
+  var num = document.getElementById("num");
+  var n = num.value;
+  if (n == "0") {
+    n = "1";
+  } else {
+    n = n + "1";
+  }
+  document.getElementById("num").value = n;
 }
